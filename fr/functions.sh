231262f1154e88e34327b3cdf7bdc01b74fc4352
PGP 2F93D5AF <wikijm@users.noreply.github.com>
2016-11-17 16:16:52 +0100

    Create functions.sh

---
+++ fr/functions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Here you can create functions which will be available from the commands file
+# You can also use here user variables defined in your config file
+sms_sendamessage = gammu sendsms TEXT $sms_phonenumber -text $sms_texttosend
